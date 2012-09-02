9b154cc948f6ab5973de207b490b68cb65d150da
vcuculo <lasek88@gmail.com>
2012-09-02 10:45:45 +0200

    Initial Commit

---
+++ ParkingFinder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ParkingFinder
+//
+//  Created by Vittorio Cuculo on 02/09/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
